b5712085da265e20ba26d66f361fdc654a16a7df
kayn.pleiades <kayn.pleiades@gmail.com>
2021-07-20 14:26:43 -0400

    very rough format of what I want the cover page to look like

---
+++ src/pages/cover.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Cover() {
+  return (
+    <div className="container-fluid min-vh-100 d-flex flex-column">
+      <div className="row flex-grow-1">
+        <div className="col-3 bg-success align-self-end">aaaa</div>
+        <div className="col-3 bg-primary align-self-center">aaaa</div>
+        <div className="col-3 bg-secondary align-self-center">aaaa</div>
+      </div>
+    </div>
+  );
+}
+
+export default Cover;
