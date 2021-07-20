20eee1b333281baea39aa1136d3b0c6c974552df
kayn.pleiades <kayn.pleiades@gmail.com>
2021-07-20 14:43:27 -0400

    started setting up router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Cover from "./pages/cover";
 
 function App() {
-  return <Cover />;
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Cover} />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
